9bd09e8668b46cffa91a99ba2faa5267454b33e1
fabiogaspar11 <82576971+fabiogaspar11@users.noreply.github.com>
2021-11-03 18:38:17 +0000

    Initial commit v2

--- laravelAPI/database/seeders/DatabaseSeeder.php
+++ laravelAPI/database/seeders/DatabaseSeeder.php
@@ -3,16 +3,45 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+    public static $seedType = "small";
+
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->command->info("-----------------------------------------------");
+        $this->command->info("START of database seeder");
+        $this->command->info("-----------------------------------------------");
+
+        DatabaseSeeder::$seedType = $this->command->choice('What is the size of seed data (choose "full" for publishing)?', ['small', 'full'], 0);
+
+        DB::statement("SET foreign_key_checks=0");
+
+        DB::table('users')->delete();
+        DB::table('default_categories')->delete();
+        DB::table('categories')->delete();
+        DB::table('payment_types')->delete();
+        DB::table('vcards')->delete();
+        DB::table('transactions')->delete();
+
+        DB::statement('ALTER TABLE users AUTO_INCREMENT = 0');
+        DB::statement('ALTER TABLE default_categories AUTO_INCREMENT = 0');
+        DB::statement('ALTER TABLE categories AUTO_INCREMENT = 0');
+        DB::statement('ALTER TABLE transactions AUTO_INCREMENT = 0');
+
+        DB::statement("SET foreign_key_checks=1");
+
+
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(PaymentTypesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(VCardsTableSeeder::class);
+        $this->call(vCardPhotosSeeder::class);
+
+        $this->command->info("-----------------------------------------------");
+        $this->command->info("END of database seeder");
+        $this->command->info("-----------------------------------------------");
     }
 }
