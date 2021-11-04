08f67fe6a0775c81e5571fa03c7120615654efb5
carla <2191170@my.ipleiria.pt>
2021-11-04 17:37:44 +0000

    Models: Category and Transaction
    Created models: Category and Transaction

---
+++ laravelAPI/app/Models/Category.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Transaction;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Category extends Model
+{
+    use HasFactory;
+    public $timestamps = true;
+
+    protected $fillable = [
+        'vcard',
+        'type',
+        'name'
+    ];
+
+
+    public function vcard()
+    {
+        return $this->belongsTo(Vcard::class, 'vcard');
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class, 'category_id');
+    }
+
+
+}
