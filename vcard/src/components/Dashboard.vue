e751f17d95e4d3dca5b486059aea35c3e45fa4ff
CcostaX <carlos.cost@live.com.pt>
2021-11-06 11:27:59 +0000

    client - dad

---
+++ vcard/src/components/Dashboard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Dashboard</h1>
+  </div>
+  <div id="dashboard" class=" justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottoms">
+    <img src="../assets/img/vcard.png"> 
+    <h1>{{moneyUser}}</h1>
+    <a id="borderOption" href="#">
+      <div class="background">
+      <h2>DashBoard</h2>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  components: {
+    
+  },
+  data() {
+    return {
+      money: 0
+    }
+  },
+  computed: {
+    moneyUser: function() {
+      return this.money + "€";
+    }
+  }
+}
+</script>
+
+
+
+<style scoped lang="scss">
+#dashboard{
+  text-align: center;
+  margin: 0 auto;
+}
+#borderOption{
+  color: white;
+  text-decoration: none;
+}
+.background{
+  background-color: gray;
+  width: 30%;
+  
+}
+
+</style>
