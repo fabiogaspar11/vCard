fa967ef6f51334c4113e37690f7ffe0245f12f4f
carla <2191170@my.ipleiria.pt>
2021-11-06 14:07:44 +0000

    Vcard - Updated controller and Request

--- laravelAPI/app/Http/Requests/VcardRequest.php
+++ laravelAPI/app/Http/Requests/VcardRequest.php
@@ -27,12 +27,30 @@ class VcardRequest extends FormRequest
             'phone_number'       => 'nullable|integer|digits:9|unique:vcards,phone_number',
             'name'               => 'required|string|max:50',
             'email'              => ['required', 'string', 'max:255'], //'regex:/^[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}$/'],
-            'photo_url'          => 'nullable|string|max:255',
-            'password'           => 'required|string|max:255',
-            'confirmation_code'  => 'required|string|max:255',
-            'blocked'            => 'required|integer|in:0,1',
-            'balance'            => ['required', 'numeric', 'min:0.00', 'regex:/^[0-9]+((.|,)[0-9]{1,2})?$/'],
-            'max_debit'          => ['required', 'numeric', 'regex:/^[0-9]+((.|,)[0-9]{1,2})?$/']
+            'photo_url'          => 'nullable|image|max:8192',
+            'password'           => 'required|string|max:255', //começar validacoes aqui
+            'confirmation_code'  => 'required|integer|digits:4',
+            'blocked'            => 'nullable|integer|in:0,1',
+            'balance'            => ['nullable', 'numeric', 'min:0.00', 'regex:/^[0-9]+((.|,)[0-9]{1,2})?$/'],
+            'max_debit'          => ['nullable', 'numeric', 'regex:/^[0-9]+((.|,)[0-9]{1,2})?$/']
+        ];
+    }
+        public function messages()
+        {
+            return [
+            'phone_number.unique' => 'A vcard is already associated with this phone number',
+            'phone_number.integer' => 'Phone number can only have numbers',
+            'phone_number.digits' => 'Phone number must have 9 digits',
+            'name.required' => 'Name is mandatory',
+            'name.max' => 'Name cannot have more than 50 characters',
+            'email.required' => 'Email is mandatory',
+            'email.max' => 'Email cannot have more than 255 characters',
+            'photo_url.image' => 'The file is not a image',
+            'password.required' => 'Password is mandatory',
+            'password.max' => 'Password cannot have more than 255 characters',
+            'confirmation_code.required' => 'Confirmation code is mandatory',
+            'confirmation_code.integer' => 'Confirmation code can only have numbers',
+            'confirmation_code.digits' => 'Confirmation code must have 4 digits',
         ];
     }
 }
