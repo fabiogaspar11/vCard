7de2bef01f490a45dc486c9bd708281b838da845
CcostaX <carlos.cost@live.com.pt>
2021-11-16 19:19:18 +0000

    aaaa

--- laravelAPI/routes/api.php
+++ laravelAPI/routes/api.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\Api\AuthController;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user()->token();
+    return $request->user();
 });
 
 /*************************************** Users ***************************************/
