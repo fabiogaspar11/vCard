d2694066736db5dee1d1d35454cdd662344356e3
fabiogaspar11 <82576971+fabiogaspar11@users.noreply.github.com>
2021-11-18 15:53:22 +0000

    Add client id dynamic var

--- laravelAPI/app/Http/Controllers/Api/AuthController.php
+++ laravelAPI/app/Http/Controllers/Api/AuthController.php
@@ -13,7 +13,7 @@ class AuthController extends Controller
             $bodyHttpRequest = [
                 'form_params' => [
                     'grant_type' => 'password',
-                    'client_id' => '3',
+                    'client_id' => env('CLIENT_ID'),
                     'client_secret' => env('CLIENT_SECRET'),
                     'username' => $request->username,
                     'password' => $request->password,
