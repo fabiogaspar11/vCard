0f0f887a7e5dea23a96ff9f1fd7328a22e1fed37
carla <2191170@my.ipleiria.pt>
2021-11-28 20:27:07 +0000

    Changed transaction resource

--- laravelAPI/app/Http/Resources/TransactionResource.php
+++ laravelAPI/app/Http/Resources/TransactionResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Vcard;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TransactionResource extends JsonResource
@@ -14,6 +15,12 @@ class TransactionResource extends JsonResource
      */
     public function toArray($request)
     {
+        if($this->pair_vcard){
+            $pair_vcard = Vcard::find($this->pair_vcard);
+        }
+        else{
+            $pair_vcard = [];
+        }
         return [
             "id"=>$this->id,
             "vcard" => $this->vcard,
@@ -25,8 +32,8 @@ class TransactionResource extends JsonResource
             "new_balance" => $this->new_balance,
             "payment_type" => $this->payment_type,
             "payment_reference" => $this->payment_reference,
-            "pair_transaction" => $this->pair_transaction,
-            "pair_vcard" => $this->pair_vcard,
+            "pair_transaction" => $this->pair_transaction,//$this->pair_transaction ? new TransactionResource($this->pair_transaction) : [],
+            "pair_vcard" => $pair_vcard != [] ? new VcardResource($pair_vcard) : $pair_vcard,
             "category_id" => $this->category,
             "description" => $this->description,
         ];
